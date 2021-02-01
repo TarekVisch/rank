de37d2900607ad6dadaf535508380c37d9be78f2
Tarek Chentouf <tarek.chentouf.99@gmail.com>
2021-02-01 19:21:56 +0100

    Change layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+const apiKey = '49bd23941e1f05cff62a609926ad2acb';
+
+async function getImage() {
+  const response = await fetch('');
+}
+
+async function getMovies() {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=49bd23941e1f05cff62a609926ad2acb&language=en-US&page=1`;
+
+  const reponse = await fetch(url);
+  const data = await reponse.json();
+
+  const mainContent = document.getElementById('main-content');
+  mainContent.innerHTML = `
+      <div class="cards">
+        ${data.results.map(movie => {
+          return `<div class="card">
+                  <p class="card-title">${movie.title}</p>
+                  <div class="card-image"></div>
+                  <p class="card-release-date">${movie.release_date}</p>
+                  <div class="card-rating">
+                    <p id="rating">${movie.vote_average != 0 ? movie.vote_average : 'NR'}</p>
+                  </div>
+                </div>`
+        }).join('')}
+      </div>
+  `;
+}
+
+getMovies().catch(err => console.error(`Error: ${err}`));
